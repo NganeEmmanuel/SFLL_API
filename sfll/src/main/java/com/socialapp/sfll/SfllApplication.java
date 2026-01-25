ca5cc12620b1259a9d6d1610e925b562d279f0ce
Ngane Emmanuel <emmanuelngane06@gmail.com>
2026-01-24 17:24:21 -0800

    initializing spring boot app adding datasource, connecting to mysql database

---
+++ sfll/src/main/java/com/socialapp/sfll/SfllApplication.java
@@ -0,0 +1,13 @@
+package com.socialapp.sfll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfllApplication.class, args);
+	}
+
+}
